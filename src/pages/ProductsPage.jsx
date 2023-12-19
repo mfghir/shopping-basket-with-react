3bda534bca2a297de1fe1b98831928c611101e38
mfghir <fatemeghafari77@gmail.com>
2023-12-18 22:21:15 -0800

    rendering products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,14 +1,20 @@
-import { useProducts } from "../context/ProductContext"
+import { useProducts } from "../context/ProductContext";
+import styles from "./ProductsPage.module.css";
 
 const ProductsPage = () => {
-
-  const products = useProducts()
+  const products = useProducts();
 
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>loading</p>}
+        {products.map((p) => (
+          <p key={p.id}>{p.title}</p>
+        ))}
+      </div>
+      <div>sidebar</div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
