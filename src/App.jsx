8e9d3e6e81b320f76f05274bd12605b65a2b0c63
mfghir <fatemeghafari77@gmail.com>
2023-12-18 21:14:06 -0800

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import NotFoundPage from "./pages/404";
+
 function App() {
   return (
-    <>
-      <h1>Fatemeweb</h1>
-    </>
+    <Routes>
+      <Route index element={<Navigate to="/products" replace />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
