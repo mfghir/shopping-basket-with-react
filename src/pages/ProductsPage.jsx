8b9a559b820803f762f9c07a74bcbcd4b0320db7
mfghir <fatemeghafari77@gmail.com>
2023-12-19 21:12:30 -0800

    add sort

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useState } from "react";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
@@ -6,11 +7,19 @@ import { useProducts } from "../context/ProductContext";
 import styles from "./ProductsPage.module.css";
 import { ImSearch } from "react-icons/im";
 
+import { FaListUl } from "react-icons/fa";
+
 const ProductsPage = () => {
   const products = useProducts();
 
   const [search, setSearch] = useState("");
   const searchHandler = () => {};
+
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase()
+    if (tagName !== "LI") return
+  };
 
   return (
     <>
@@ -33,7 +42,20 @@ const ProductsPage = () => {
             <Card key={p.id} data={p} />
           ))}
         </div>
-        <div>sidebar</div>
+
+        <div>
+          <div className="">
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelry</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
       </div>
     </>
   );
