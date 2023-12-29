ba75dd580dd02e283600eecc4a310321114ac120
mfghir <fatemeghafari77@gmail.com>
2023-12-29 09:34:46 +0330

    add store

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './global.css'
+
 import { BrowserRouter } from 'react-router-dom'
+import store from './app/store.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
     </BrowserRouter>
   // </React.StrictMode>,
 )
